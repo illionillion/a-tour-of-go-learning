c05a9eab0621b189041a1515f6d821e1015c7f9d
yuto ichinose <yuto.0526.sano@gmail.com>
2025-02-25 18:36:24 +0900

    first commit

---
+++ basics-02/import.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	// math.Sqrtで平方根を返す
+	fmt.Printf("Now you have %g problems.\n", math.Sqrt(7))
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/illionillion/a-tour-of-go-learning
+
+go 1.22.2
