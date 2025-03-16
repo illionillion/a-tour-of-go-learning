77a79260200e903c59440c3bd45935dad4384047
yuto ichinose <yuto.0526.sano@gmail.com>
2025-03-16 22:44:34 +0900

    docs: コメント追加

--- moretypes-01/pointers.go
+++ moretypes-01/pointers.go
@@ -12,6 +12,7 @@ func main() {
 	fmt.Println(*p) // read i through the pointer
 	*p = 21         // set i through the pointer
 	fmt.Println(i)  // see the new value of i
+	// pはiをシャローコピーしてるからiも同じ21になる
 
 	p = &j         // point to j
 	*p = *p / 37   // divide j through the pointer
