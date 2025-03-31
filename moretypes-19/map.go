6a655419ca4ceab2723296b7d14da293357847b5
yuto ichinose <yuto.0526.sano@gmail.com>
2025-04-01 06:30:18 +0900

    feat: マップに新しい地点情報を追加

--- moretypes-19/map.go
+++ moretypes-19/map.go
@@ -7,6 +7,10 @@ type Vertex struct {
 }
 
 var m map[string]Vertex
+var m2 = map[string]Vertex{
+	"Bell Labs": {40.68433, -74.39967},
+	"Google":    { Lat: 37.42202, Long: -122.08408},
+}
 
 func main() {
 	m = make(map[string]Vertex)
@@ -19,4 +23,9 @@ func main() {
 	for name, vertex := range m {
 		fmt.Printf("%s: %+v\n", name, vertex)
 	}
+
+	for name, vertex := range m2 {
+		fmt.Printf("%s: %+v\n", name, vertex)
+	}
+
 }
