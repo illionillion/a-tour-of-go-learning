0ab354f71be71d3929eba66dfc75800a4cfebfc8
yuto ichinose <yuto.0526.sano@gmail.com>
2025-04-08 07:53:36 +0900

    feat: Vertex型の新しい関数Abs2を追加

--- methods-01/methods.go
+++ methods-01/methods.go
@@ -14,8 +14,15 @@ func (v Vertex) Abs() float64 {
 	return math.Sqrt(v.X*v.X + v.Y*v.Y)
 }
 
+// 普通のやつ
+func Abs2(v Vertex) float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
 func main() {
 	v := Vertex{3, 4}
 	// v Vertex型のメソッドを呼び出す
 	fmt.Println(v.Abs())
+	// 普通に呼び出す
+	fmt.Println(Abs2(v))
 }
